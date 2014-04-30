363da0093e0333eac9fadbdf912b7e0f9bf751e3
zoom <zoom@zoomtekiMac.local>
2014-04-30 14:27:09 -0700

    Initial Commit

---
+++ Photo View/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photo View
+//
+//  Created by zoom on 14-4-30.
+//  Copyright (c) 2014年 Jiance Tong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Photo ViewTests/Photo_ViewTests.m
@@ -0,0 +1,34 @@
+//
+//  Photo_ViewTests.m
+//  Photo ViewTests
+//
+//  Created by zoom on 14-4-30.
+//  Copyright (c) 2014年 Jiance Tong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Photo_ViewTests : XCTestCase
+
+@end
+
+@implementation Photo_ViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
